d0b31c799ebae0e805b399d1545a4b5996ab5ed9
Ryan Grunest <ryangrunest@geeko>
2021-09-24 11:39:21 -0700

    use input text from one component to update beforeTaxPrice variable, and then update another component with that value

--- SalesTaxCalculator/ViewController.swift
+++ SalesTaxCalculator/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var afterTaxRateComponent: UITextField!
+    var beforeTaxPrice: Float = 0
+    
+    func updateView(input: String) {
+        if let value = Float(input) {
+            beforeTaxPrice = value
+            afterTaxRateComponent.text = String(describing: value)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateView(input: "412")
     }
 
-
+    @IBAction func beforeTaxPriceChange(_ sender: UITextField) {
+        updateView(input: sender.text ?? "")
+    }
 }
 
