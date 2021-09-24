c7301f32d828b44e85f4cb82b435dcdd76e62a9e
Ryan Grunest <ryangrunest@geeko>
2021-09-24 09:35:29 -0700

    init repo

---
+++ SalesTaxCalculator/SalesTaxCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTaxCalculator
+//
+//  Created by Ryan Grunest on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
